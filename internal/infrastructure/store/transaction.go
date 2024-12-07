3b0fe94e8bd656535bdfd2f7765049e3bad61b87
roman.kazarov <roman.kazarov@gett.com>
2024-12-07 20:21:16 +0100

    add transaction store

---
+++ internal/infrastructure/store/transaction.go
@@ -0,0 +1,39 @@
+package store
+
+import (
+	"sync"
+
+	"github.com/KRR19/EthereumParser/internal/models"
+)
+
+type TransactionStore struct {
+	data map[string]models.Transaction
+	mu   *sync.RWMutex
+}
+
+func NewTransactionStore() *TransactionStore {
+	return &TransactionStore{
+		data: make(map[string]models.Transaction),
+		mu:   &sync.RWMutex{},
+	}
+}
+
+func (s *TransactionStore) Save(transactions models.Transaction) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[transactions.Hash] = transactions
+}
+
+func (s *TransactionStore) GetTransactions(hash ...string) []models.Transaction {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	 transactions := make([]models.Transaction, 0, len(hash))
+	for _, h := range hash {
+		if t, ok := s.data[h]; ok {
+			transactions = append(transactions, t)
+		}
+	}
+
+	return transactions
+}
